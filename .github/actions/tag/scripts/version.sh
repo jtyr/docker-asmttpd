ab9b27733df2382990f31fc2fd8fdc25c2de6ab1
Jiri Tyr <jtyr@users.noreply.github.com>
2025-02-23 22:40:03 +0000

    Fixing action (#11)

--- .github/actions/tag/scripts/version.sh
+++ .github/actions/tag/scripts/version.sh
@@ -6,13 +6,22 @@ set -o pipefail
 ASMTTPD_VERSION=$(grep -Po '^ARG asmttpd_version=\K.*' Dockerfile)
 FILE_VERSION=$(cat .version)
 
-VERSION="$FILE_VERSION+$ASMTTPD_VERSION"
+DOCKER_TAG="$FILE_VERSION-$ASMTTPD_VERSION"
+GIT_TAG="$FILE_VERSION+$ASMTTPD_VERSION"
 
-echo "I: VERSION=$VERSION"
+echo "I: ASMTTPD_VERSION=$ASMTTPD_VERSION"
+echo "I: FILE_VERSION=$FILE_VERSION"
+echo "I: DOCKER_TAG=$DOCKER_TAG"
+echo "I: GIT_TAG=$GIT_TAG"
 
 if [[ -n $CI ]]; then
-    echo "version=$VERSION" >> "$GITHUB_OUTPUT"
+    {
+        echo "asmttpd_version=$ASMTTPD_VERSION"
+        echo "file_version=$FILE_VERSION"
+        echo "docker_tag=$DOCKER_TAG"
+        echo "git_tag=$GIT_TAG"
+    } >> "$GITHUB_OUTPUT"
 fi
 
-git tag "$VERSION"
+git tag "$GIT_TAG"
 git push --tags
